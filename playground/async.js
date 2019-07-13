6332f633cdf1091c5150583f01dc2c522cd90e85
Your Name <rakibsarwar3@outlook.com>
2019-07-13 22:07:22 +0600

    Asyncrounus Basic

---
+++ playground/async.js
@@ -0,0 +1,15 @@
+console.log('Starting App');
+
+setTimeout(()=>{
+    console.log('This will come after 200 ms')
+},200);
+
+
+setTimeout(()=>{
+    console.log('this will come out in 0 ms')
+},0)
+
+
+
+
+console.log('finishing app')
